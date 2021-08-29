7cc5885c66192b28478c2850c9b9a60fd0f9a34b
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-08-29 09:30:33 +0200

    scoped production class names

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import { Landing, Pricing } from './components';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 export const App = () => {
   return (
-    <div>
-      <StylesProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path='/pricing' component={Pricing} />
-            <Route path='/' component={Landing} />
-          </Switch>
-        </BrowserRouter>
-      </StylesProvider>
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/pricing' component={Pricing} />
+          <Route path='/' component={Landing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
